c7f9c2e9dd1a3228dc23c698a7e1bcce6a50d8b2
Olga <olga.dubrovina.olga@gmail.com>
2023-01-12 13:48:49 +0300

    TestBase

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SearchTest {
+
+    @Test
+    void searchGoogle() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("Selenide").pressEnter();
+        $("#search").shouldHave(text("selenide.org"));
+    }
+
+    
+
+}
