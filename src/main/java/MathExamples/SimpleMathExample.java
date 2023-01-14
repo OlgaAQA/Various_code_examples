d19bf1340c3679c8233c1ac67e2de1c9e679e6ad
Olga <olga.dubrovina.olga@gmail.com>
2023-01-14 15:09:38 +0300

    delete math

--- src/main/java/MathExamples/SimpleMathExample.java
+++ src/main/java/MathExamples/SimpleMathExample.java
@@ -2,7 +2,7 @@ package MathExamples;
 
 import java.util.Scanner;
 
-public class MathOne{
+public class SimpleMathExample {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         float a = sc.nextInt();
@@ -10,6 +10,7 @@ public class MathOne{
         float c = sc.nextInt();
         float result = (a + b - 3) * c / 2 % 10; // Показывает последнее число вычисленного результата
         //float result = a / b ;
+        System.out.println((a % 10) + (a / 10) % 10 + (a / 100)); // Сумма всех числе трехзначного числа
         System.out.println(result);
     }
 }
