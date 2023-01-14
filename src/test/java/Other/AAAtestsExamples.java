f8239ada60793cadf4f5ccefea6cc8004e3fc7a4
Olga <olga.dubrovina.olga@gmail.com>
2023-01-14 15:01:23 +0300

    +AAA test

---
+++ src/test/java/Other/AAAtestsExamples.java
@@ -0,0 +1,36 @@
+package Other;
+
+
+import org.junit.jupiter.api.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.*;
+
+public class AAAtestsExamples {
+
+    @Test
+    void shouldFindSelenideRepositoryInGithub() {
+        // открыть страницу github.com
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать Enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        // нажимаем на линк от первого результата поиска
+        $$("ul.repo-list li").first().$("a").click();
+        // check: в заголовке встречается selenide/selenide
+        $("h1").shouldHave(text("selenide / selenide"));
+
+        webdriver().shouldHave(url("saf"));
+
+        //ARRANGE (optional)
+        //ACT
+        //ASSERT
+    }
+
+    @Test
+    void searchGoogle() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("Selenide").pressEnter();
+        $("#search").shouldHave(text("selenide.org"));
+    }
+
+}
