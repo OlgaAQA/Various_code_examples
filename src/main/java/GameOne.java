5730805cdaaf60e3e6c028f62f57b67185db0036
Olga <olga.dubrovina.olga@gmail.com>
2023-01-22 16:39:35 +0300

    +++

--- src/main/java/GameOne.java
+++ src/main/java/GameOne.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class GameOne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ttt = (int) (Math.random() * 100 + 1);
+        int ttt = (int) (Math.random() * 10 + 1);
         System.out.println(ttt);
-        int guess = 0;
+        int ggg = 0;
         System.out.println("Добро пожаловать в игру Тренировка Интуиции!!!");
         System.out.println("Отгадай какое число от 1 до 100 загаданно:");
-        guess = sc.nextInt();
-        System.out.println("Вы ввели:" + guess +".");
+        while (ggg != ttt) {
+            ggg = sc.nextInt();
+            System.out.println("Вы ввели:" + ggg + ".");
+            if (ggg > ttt)
+                System.out.println("Загаданное число меньше! Попробуй снова:");
+
+            else if (ggg > ttt)
+                System.out.println("Загаданное число больше! Попробуй снова:");
+            else
+                System.out.println(ggg + "Ты отгадал!!! Поздравляю! У тебя отличная интуиция!");
+        }
     }
 
 }
