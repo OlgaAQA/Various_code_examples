eab757a191565a4823ed812496ec9ee17f250858
Olga <olga.dubrovina.olga@gmail.com>
2023-02-08 20:35:32 +0300

    +++randomUtils

--- src/test/java/testExamples/tests/TestData.java
+++ src/test/java/testExamples/tests/TestData.java
@@ -1,9 +1,9 @@
 package testExamples.tests;
 
-import static testExamples.utils.RandomUtilsExamples.getRandomNumberString;
+import static testExamples.utils.RandomUtils.getRandomNumberString;
 
 public class TestData {
     public static String email = "User@gmail.com";
-    public static String textAddress ="Text address";
+    public static String textAddress = "Text address";
     public static String mobileRandomNumber = getRandomNumberString(10);
 }
---
+++ src/test/java/testExamples/utils/RandomUtilsExample.java
@@ -0,0 +1,24 @@
+package testExamples.utils;
+
+import static testExamples.utils.RandomUtils.*;
+
+public class RandomUtilsExample {
+    public static void main(String[] args) {
+        System.out.println(getRandomString(10));
+
+        int randomInt = getRandomInt(223333, 100000000);
+        System.out.println(randomInt + "");
+
+        String randomPhone = getRandomPhone();
+        System.out.println(randomPhone);
+
+        String randomPhoneWithPrefix = getRandomPhone("+7");
+        System.out.println(randomPhoneWithPrefix);
+
+        String difficultRandomPhoneWithPrefix = getDifficultRandomPhone("+8");
+        System.out.println(difficultRandomPhoneWithPrefix);
+
+        String randomEmail = getRandomEmail();
+        System.out.println(randomEmail);
+    }
+}
