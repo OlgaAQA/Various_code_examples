338950fddc861193d9406f8f5c72b4b9e42b5567
Olga <olga.dubrovina.olga@gmail.com>
2023-03-22 14:54:18 +0300

    +

--- src/test/java/testExamples/pages/ExElementsPage.java
+++ src/test/java/testExamples/pages/ExElementsPage.java
@@ -1,17 +1,50 @@
 package testExamples.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import io.qameta.allure.Step;
 import testExamples.tests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ExElementsPage extends TestBase {
+
     // locators & elements
-    public static SelenideElement formTitle = $(".main-header");
+    static Faker faker = new Faker();
+    public static SelenideElement formTitle = $(".main-header"),
+            userName = $("#userName"),
+            userEmail = $("#userEmail");
+    public String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName();
+
     // actions
+    @Step("Переход к неоюходимой странице")
+    public ExElementsPage openPageTextBox(String url) {
+        openPage(url);
+        takeScreenshot();
+        return this;
+    }
+
+    @Step("Проверка наименования страницы")
     public ExElementsPage checkOpenPage(String FORM_TITLE) {
         formTitle.shouldHave(text(FORM_TITLE));
         return this;
     }
+
+    @Step("Ввод рандомного имя пользователя")
+    public ExElementsPage setUserName() {
+        userName.setValue(firstName);
+        userName.append(" ");
+        userName.append(lastName);
+        return this;
+    }
+
+    @Step("Ввод рандомного емейла")
+    public ExElementsPage typeUserEmail(String email) {
+        userEmail.setValue(email);
+        return this;
+    }
+
+
 }
--- src/test/java/testExamples/tests/ExElements.java
+++ src/test/java/testExamples/tests/ExElements.java
@@ -9,30 +9,37 @@ import java.io.File;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
+import static testExamples.tests.TestData.emailRandom;
 
 public class ExElements extends TestBase {
-ExElementsPage exElementsPage = new ExElementsPage();
+    ExElementsPage exElementsPage = new ExElementsPage();
 
     @Test
     void testBox() {
 
-        step("Открытие страницы Text Box", () -> {
-   openPage("https://demoqa.com/text-box");
- exElementsPage.checkOpenPage("Text Box");
- takeScreenshot();
+
+        exElementsPage.openPageTextBox("https://demoqa.com/text-box")
+                .checkOpenPage("Text Box")
+                .setUserName()
+                .typeUserEmail(emailRandom);
+        //Заполнения первый адрес
+        $("#currentAddress").setValue("FUUUU");
+        //Заполнение второй адрес
+        $("#permanentAddress").setValue("FUUUU");
+        // Спбмит
+        $("#submit").click();
+        //Проверка
 
 
-        });
-
-    }
+}
 
     @Test
     void checkBox() {
 
         step("Открытие страницы Check Box", () -> {
-        openPage("https://demoqa.com/checkbox");
-        exElementsPage.checkOpenPage("Check Box");
-        takeScreenshot();
+            openPage("https://demoqa.com/checkbox");
+            exElementsPage.checkOpenPage("Check Box");
+            takeScreenshot();
 
 
         });
@@ -43,9 +50,9 @@ ExElementsPage exElementsPage = new ExElementsPage();
     void radioButton() {
 
         step("Открытие страницы Radio Button", () -> {
-        openPage("https://demoqa.com/radio-button");
-        exElementsPage.checkOpenPage("Radio Button");
-        takeScreenshot();
+            openPage("https://demoqa.com/radio-button");
+            exElementsPage.checkOpenPage("Radio Button");
+            takeScreenshot();
 
 
         });
@@ -56,9 +63,9 @@ ExElementsPage exElementsPage = new ExElementsPage();
     void webTables() {
 
         step("Открытие страницы Web Tables", () -> {
-        openPage("https://demoqa.com/webtables");
-        exElementsPage.checkOpenPage("Web Tables");
-        takeScreenshot();
+            openPage("https://demoqa.com/webtables");
+            exElementsPage.checkOpenPage("Web Tables");
+            takeScreenshot();
 
 
         });
@@ -69,9 +76,9 @@ ExElementsPage exElementsPage = new ExElementsPage();
     void clickButton() {
 
         step("Открытие страницы Buttons", () -> {
-        openPage("https://demoqa.com/buttons");
-        exElementsPage.checkOpenPage("Buttons");
-        takeScreenshot();
+            openPage("https://demoqa.com/buttons");
+            exElementsPage.checkOpenPage("Buttons");
+            takeScreenshot();
 
 
         });
@@ -88,12 +95,12 @@ ExElementsPage exElementsPage = new ExElementsPage();
 //        });
 
         step("Открытие страницы Upload the-internet.herokuapp.com", () -> {
-                openPage("https://the-internet.herokuapp.com/upload");
-              $(".example").shouldHave(text("File Uploader"));
-                takeScreenshot();
+            openPage("https://the-internet.herokuapp.com/upload");
+            $(".example").shouldHave(text("File Uploader"));
+            takeScreenshot();
         });
 
-            step("Загрузка файла по относительному пути", () -> {
+        step("Загрузка файла по относительному пути", () -> {
             File exampleFile = new File("src/test/resources/img/catrider.jpg");
             $("input[type='file']").uploadFile(exampleFile);
             $("#file-submit").click();
